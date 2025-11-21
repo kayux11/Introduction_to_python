00fd618e992f25f6e9e971dc7e8eb976728ca46a
abdulkarim sharafudeen adegboye <abdulkarimsarafadeen@gmail.com>
2025-11-21 09:16:59 +0100

    BMI simple formula, temp converter, average and area of a circle

--- Arithmetics/exercise.py
+++ Arithmetics/exercise.py
@@ -9,3 +9,28 @@ print(f"difference of {number1} and {number2} is {diff_result}")
 print(f"qoutient of {number1} and {number2} is {divid_result}")
 print(f"multiplication of {number1} and {number2} is {mult_result}")
 
+
+# BMI Calculation
+weight = float(input("Enter your weight: "))
+height = float(input("Enter your height: "))
+
+print("Your BMI is", weight / (height ** 2))
+
+
+# ConvertbCelcius to Fahreheit
+celcius = float(input("Enter temperature in Celcius: ",))
+
+print("Equivalent Temperature in Fahreheit is", (celcius * 9/5) + 32 )
+
+# Average of Number
+num1 = float(input("Enter the first number: ",))
+num2 = float(input("Enter the second number: ",))
+num3 = float(input("Enter the third number: ",))
+sum = num1 + num2 + num3
+
+print(f"Average is", sum/3)
+
+
+# Area of a circle
+r = 2
+print("Area of a circle is", 3.142*(r**2))
