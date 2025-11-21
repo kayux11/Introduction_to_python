d2e3fb3ecd7712d2ec195de8d1f051607abd33ee
abdulkarim sharafudeen adegboye <abdulkarimsarafadeen@gmail.com>
2025-11-21 07:31:42 +0100

    Basic arithmetic using f-string

---
+++ Arithmetics/exercise.py
@@ -0,0 +1,11 @@
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+sum_result = number1 + number2
+diff_result = number1 - number2
+divid_result = number1 / number2
+mult_result = number1 * number2
+print(f"sum of {number1} and {number2} is {sum_result} ")
+print(f"difference of {number1} and {number2} is {diff_result}")
+print(f"qoutient of {number1} and {number2} is {divid_result}")
+print(f"multiplication of {number1} and {number2} is {mult_result}")
+
